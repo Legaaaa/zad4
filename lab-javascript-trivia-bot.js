0a1457649232dc3dc111c6dad02a8b0906f6d3e4
Legaaaa <kamil300104@wp.pl>
2025-06-01 09:45:09 +0200

    Update lab-javascript-trivia-bot.js

--- lab-javascript-trivia-bot.js
+++ lab-javascript-trivia-bot.js
@@ -2,16 +2,15 @@
 
 console.log("Hello! I'm your coding fun fact guide!");
 
-// Bot information
+
 let botName = "CodeBot";
 let botLocation = "the cloud";
 let favoriteLanguage = "JavaScript";
 
-// Introduction
+
 console.log("My name is " + botName + " and I live on " + botLocation + ".");
 console.log("My favorite programming language is " + favoriteLanguage + ".");
 
-// Fun facts
 let codingFact = favoriteLanguage + " was originally created in just 10 days by Brendan Eich in 1995.";
 console.log(codingFact);
 
@@ -21,7 +20,7 @@ console.log(codingFact);
 codingFact = favoriteLanguage + " can be used for both frontend and backend development.";
 console.log(codingFact);
 
-// Farewell message
+
 console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
 
 
